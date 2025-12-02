71ca7aa6e2a7d9a039baa1065212b810f41e39e7
Abdullah Sulimani <abdullah.sulimani@stud.h-da.de>
2025-12-02 14:01:30 +0100

    Implement method to extract number of products

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
 import os
 import glob
-# import requests (requests might still be needed for API calls to download images/files/json)
+from typing import Any
 
 url = 'https://www.cannabisdarmstadt.de'
 
@@ -21,6 +22,7 @@ def fetch_page_html(driver: webdriver.Edge, url: str) -> str:
     time.sleep(3) # wait for dynamic content to load
     return driver.page_source
 
+# When the task is to fetch all the products, you're gonna have to navigate a bit with Webdriver and then save the results somehow
 def store_page_html(page_html: str, folder: str = 'stored_html') -> str:
     os.makedirs(folder, exist_ok=True)
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
@@ -40,6 +42,16 @@ def parse_html_file(path: str) -> BeautifulSoup:
     with open(path, encoding='utf-8') as f:
         return BeautifulSoup(f.read(), 'html.parser')
 
+# Very hacky and non-maintainable solution
+# Performance: probably bad; you have to search for "Produkte gefunden" every time
+# Maintainability: This string is unlikely to be anywhere else on the site. The site's template is unlikely to change
+def scrape_num_products(soup: BeautifulSoup) -> str:
+    produkte_gefunden_string = soup.find(string='Produkte gefunden')
+    num_products_parent = produkte_gefunden_string.parent
+    num_products_parent_content = num_products_parent.get_text()
+    extracted_integer = num_products_parent_content[0:2]
+    return extracted_integer
+
 if __name__ == '__main__':
     fetch_new_page = False
 
@@ -48,12 +60,28 @@ if __name__ == '__main__':
         try:
             html = fetch_page_html(driver, url)
             filepath = store_page_html(html)
-            print(f"Saved HTML to {filepath}")
+            print(f'Saved HTML to {filepath}')
+
+            # Use By.XPATH
+            # When scraping products: use webdriver to load in next page
         finally:
             driver.quit()
     else:
         filepath = get_latest_snapshot()
-        print('Reading latest saved HTML: {filepath}')
+        print(f'Reading latest saved HTML: {filepath}')
 
-    soup = parse_html_file('filepath')
-    print(soup.title)
+        soup = parse_html_file(filepath)
+
+        # test = soup.find_all('p')
+        # for tag in test[8]:
+        #     print(f'Name: {tag.name}, Content: {tag.get_text()}')
+
+        num_products = scrape_num_products(soup)
+        print(f'Number of products: {num_products}')
+
+        # driver.get('file://C:/Users/abdul/PycharmProjects/CannabisWebScraper/stored_html/snapshot_2025-12-01_21-22.html')
+        # print(driver.find_elements(By.CLASS_NAME, 'MuiTypography-root.MuiTypography-body1.mui-9mqwrs'))
+
+
+    # soup = parse_html_file('filepath')
+    # print(soup.title)
