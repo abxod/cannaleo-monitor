48df69c6690349ab9be46c842e32bb856a1389da
abxod <abxod@users.noreply.github.com>
2026-02-27 21:53:25 +0000

    deploy: 3671335e6471aa9c5937a94a15247b94115b6dd4

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 export const SUPABASE_URL = "https://cjhpqurdralszprormcp.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqaHBxdXJkcmFsc3pwcm9ybWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2OTIyNzAsImV4cCI6MjA4MDI2ODI3MH0.5X-tXFPe433dAGFsE_RkYdRgpTqRbigCiJHiw86_H0c";
 export const BUCKETS = {
-  products: "all_products_bucket",
-  vendors: "vendors_info_bucket",
-  inventories: "inventories_bucket"
+  products: "pid-to-info",
+  vendors: "vendor-id-to-info",
+  inventories: "inventories"
 };
 export const FILES = {
-  products: "all_current_products.json",
-  vendors: "vendors_information.json",
-  inventories: "vendors_inventories.json"
+  products: "pid_to_info.json",
+  vendors: "vendor_id_to_info.json",
+  inventories: "vendor_id_to_offers.json",
+  pidToVendors: "pid_to_vendor_offers.json"
 };
