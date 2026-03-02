c227d5f3ffd78164487f9fb8c95494c82108cf17
abxod <abxod@users.noreply.github.com>
2026-03-02 14:31:58 +0000

    deploy: d93fee0edb7ebfc49669dc926d65cca825a1f7e5

---
+++ functions/get_best_thc_price_ratios.js
@@ -0,0 +1,51 @@
+import {STRAIN_TYPE_NORMALIZATION_MAP} from "../utils.js";
+
+export function getBestThcPriceRatios(products, pidToVendors, filteredVendorIds, availabilityFilter, strainFilter, productCount) {
+    // TODO: Caller of this function has to sanitize the inputs of availabilityOptions, strainTypes, productCount. First two should be check-boxes, productCount and maxDistance a slider/textbox
+    // TODO: maxDistance should only be calculated once the function is executed.
+    // TODO: Get users' coordinates: Display error if user coordinates not available. If user presses a button that prompts him for his location after which his coordinates are calculated, then function can proceed.
+    // TODO: Should we filter
+    const bestRatios = []
+
+    for (const [pid, offers] of Object.entries(pidToVendors)) {
+        const product = products[pid];
+        if (!product) continue;
+
+        // Discard non-desired strains
+        const key = `${product.genetic}|${product.dominance}`;
+        const strainType = STRAIN_TYPE_NORMALIZATION_MAP[key];
+        if (!strainFilter.has(strainType)) continue;
+
+        // Discard incorrectly categorid CBD products
+        if (product.thc < 10 || product.cbd > 1) continue;
+
+        // Find best valid offer for this PID
+        let bestOffer = null;
+        for (const offer of offers) {
+            if (!filteredVendorIds.has(offer.vendor_id)) continue;
+            if (!availabilityFilter.has(offer.availability)) continue;
+
+            if (bestOffer == null || offer.price < bestOffer.price) {
+                bestOffer = offer;
+            }
+        }
+
+        if (bestOffer == null) continue;
+
+        if (bestOffer.price === 0) continue;
+
+        const ratio = product.thc / bestOffer.price;
+        bestRatios.push({
+            ratio,
+            pid,
+            name: product.name,
+            vendorId: bestOffer.vendor_id,
+            price: bestOffer.price,
+            thc: product.thc,
+            availability: bestOffer.availability
+        });
+    }
+
+    bestRatios.sort((a, b) => b.ratio - a.ratio);
+    return bestRatios.slice(0, productCount);
+}
