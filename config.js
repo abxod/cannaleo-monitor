d94ef6b5a327e53baa29c7a61e86524124b6f1a2
abxod <abxod@users.noreply.github.com>
2026-03-19 14:03:20 +0000

    deploy: 7bfef347c360023f6bbcbd1bb363a4f0299dfa8f

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const SUPABASE_URL = "https://cjhpqurdralszprormcp.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqaHBxdXJkcmFsc3pwcm9ybWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2OTIyNzAsImV4cCI6MjA4MDI2ODI3MH0.5X-tXFPe433dAGFsE_RkYdRgpTqRbigCiJHiw86_H0c";
+export const DASHBOARD_PASSWORD = "Y3ye4SkVlpbY7Ixx";
 export const BUCKETS = {
   products: "pid-to-info",
   vendors: "vendor-id-to-info",
